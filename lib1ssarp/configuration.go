2c65a074bf52ba8dbe48980fb67ae1fea657dddb
vench <nowwrit@mail.ru>
2017-08-27 16:09:57 +0300

    0827 add

--- lib1ssarp/configuration.go
+++ lib1ssarp/configuration.go
@@ -17,6 +17,22 @@ func (c Configuration) String () string {
 	return fmt.Sprintf("Configuration{Language: %s, Version: %s}", c.Language, c.Version)
 }
 
+type Models struct {
+	models []Model
+}
+
+type Model struct {
+	Name string
+	fields []Field
+}
+
+type Field struct {
+	Name string
+	Type string
+	Autoincrement bool
+	Length uint
+}
+
 
 type ConfigurationBuilder interface {
 	Build(c Configuration)
